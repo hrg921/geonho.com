29624e321d22dff93cd485d54de05e23fd53a80e
hrg921 <hrg921@gmail.com>
2019-04-10 11:35:19 +0900

    configure apollo client, add sample fragment

--- private/src/App.tsx
+++ private/src/App.tsx
@@ -3,16 +3,17 @@ import { ApolloProvider } from "react-apollo";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import MakerRoutes, { basePath as makerBasePath } from './routes/MakerRoutes';
-import MoneyFlowRoutes, { basePath as moneyFlowBasePath } from './routes/MoneyFlowRoutes';
+import MoneyFlowRoutes, {
+  basePath as moneyFlowBasePath,
+} from './routes/MoneyFlowRoutes';
 import { client } from './stores/apollo';
 
-const moneyFlowRoute = <Route path={moneyFlowBasePath} exact component={MoneyFlowRoutes} />;
+const moneyFlowRoute = (
+  <Route path={moneyFlowBasePath} exact component={MoneyFlowRoutes} />
+);
 const makerRoute = <Route path={makerBasePath} component={MakerRoutes} />;
 
-const routes = [
-  makerRoute,
-  moneyFlowRoute
-];
+const routes = [makerRoute, moneyFlowRoute];
 
 class App extends React.Component {
   render() {
@@ -21,7 +22,7 @@ class App extends React.Component {
         <Router>
           <Switch>
             {routes}
-            <Route path='/' component={MakerRoutes} />
+            <Route path="/" component={MakerRoutes} />
           </Switch>
         </Router>
       </ApolloProvider>
